2bafb8c8f8411d5ddd4af1baf393b7a0ed56a38f
Andrew <expandrew@gmail.com>
2017-10-06 07:33:56 -0700

    build out equipment functionality.

---
+++ Assets/Scripts/Items/Equipment.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Equipment", menuName = "Inventory/Equipment")]
+public class Equipment : Item {
+
+    public EquipmentSlot equipSlot;
+    public SkinnedMeshRenderer mesh;
+
+    public int armorModifier;
+    public int damageModifier;
+
+    public override void Use()
+    {
+        base.Use();
+        EquipmentManager.instance.Equip(this);
+        RemoveFromInventory();
+    }
+
+}
+
+public enum EquipmentSlot {  Head, chest, Legs, Weapon, Shield, Feet }
